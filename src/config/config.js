994755b61f7419fe6fe0ec0619028e47f59ceab6
Cristopher Rondon Añaños <89040296+Cris200399@users.noreply.github.com>
2025-02-23 20:15:34 -0500

    code: add backend url

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,7 @@ import getApiEndpoints from "@/config/apiEndpoints";
 
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5001/api';
-const API_BASE_URL = 'http://localhost:5001/api';
+// const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://academiabackend-en0f.onrender.com/api';
 export const API_URLS = getApiEndpoints(API_BASE_URL);
 export const api = createAxiosInstance(API_BASE_URL);
