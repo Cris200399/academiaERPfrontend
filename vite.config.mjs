7e5a26388209f198063c80f0463edd68ec56ac4b
Cristopher Rondon Añaños <89040296+Cris200399@users.noreply.github.com>
2025-03-04 11:44:29 -0500

    code: fix visualizer

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import { compression } from 'vite-plugin-compression2';
-import visualizer from 'rollup-plugin-visualizer';
+import {visualizer} from 'rollup-plugin-visualizer';
 
 export default defineConfig({
     plugins: [
